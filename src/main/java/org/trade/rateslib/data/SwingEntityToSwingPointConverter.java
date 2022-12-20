4cdc097129d61df20e2af8ab901191422536ebaa
Andrey Ivanov <a83ivanov@gmail.com>
2022-12-20 21:11:01 +0300

    Commit

---
+++ src/main/java/org/trade/rateslib/data/SwingEntityToSwingPointConverter.java
@@ -0,0 +1,40 @@
+package org.trade.rateslib.data;
+
+import org.trade.rateslib.model.SwingDirection;
+import org.trade.rateslib.model.SwingPoint;
+import org.trade.rateslib.utils.SwingLengthCalculator;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * @author javonavi
+ */
+public class SwingEntityToSwingPointConverter {
+
+    public static SwingPoint convert(SwingEntity swingEntity,
+                                     String stock,
+                                     String timeframe,
+                                     LocalDateTime previousSwingTime,
+                                     RatesService ratesService) {
+        double length = SwingLengthCalculator.calcTimeBetween(timeframe,
+                previousSwingTime,
+                swingEntity.getTime());
+        double lengthInBars = SwingLengthCalculator.calcBarsBetween(ratesService,
+                stock,
+                timeframe,
+                previousSwingTime,
+                swingEntity.getTime(),
+                timeframe);
+                ratesService.getCountBetween(stock, timeframe, swingEntity.getTime(), previousSwingTime);
+        return SwingPoint.builder()
+                .withDirection(SwingDirection.byBoolean(swingEntity.getDirection()))
+                .withLength(length)
+                .withLengthInBars(lengthInBars)
+                .withPrice(BigDecimal.valueOf(swingEntity.getPrice()))
+                .withSection(0)
+                .withTime(swingEntity.getTime())
+                .withTimeframe(timeframe)
+                .build();
+    }
+}
